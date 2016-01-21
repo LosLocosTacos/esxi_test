9da295dd3af9fddff1b537dcb6fecadb3c7af017
LosLocosTacos <sburns02@gmail.com>
2016-01-21 16:48:43 -0700

    Updated to include functions and better coding

--- esxi_vm_import.ps1
+++ esxi_vm_import.ps1
@@ -12,71 +12,126 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Adds the base PowerCLI cmdlets
+Add-PSSnapin VMware.VimAutomation.Core
+
 # Default variables
-$vmHost = "192.168.0.20"
-$vmDatastore = "datastore2"
-$vmDiskFormat = "Thin"
-$vmCsvFile = "D:\test.csv"
-$vmOvf = "D:\vm_images\centos5\centos5.ovf"
-$vmGuestLogin = "root"
-$vmGuestPassword = "password1234"
+$csvfile = "D:\test.csv"
+$vmhost  = "192.168.1.30"
+$ds      = "datastore2"
+$df      = "Thin"
+$ovf     = "D:\vm_images\centos5\centos5.ovf"
+$gu      = "root"
+$gp      = "testlab2016"
 
 # VM Startup timeout in seconds
 $maxTimeOut = 300
 
+Function Deploy-OVF{
+    param($virtualmachine)
+    $vm = $virtualmachine
+    $vmstatus = Get-VM $vm -ErrorAction 0
+    if($vmstatus -ne ""){
+        Write-Host "Deploying OVF Template $ovf to Virtual Machine name $vm" -fore Yellow
+        Import-VApp -Source $ovf -Name $vm -VMHost $vh -Datastore $ds -DiskStorageFormat $df
+    }
+    else{
+        Write-Host $vm "already exists" -fore Red
+    }
+}
+
+Function Start-VM{
+    param($virtualmachine)
+    $vm = Get-VM $virtualmachine -ErrorAction 0
+    if($vm.powerstate -ne "PoweredOn"){
+        # Start the Virtual Machine
+        Write-Host "Starting $vm" -fore Yellow
+        $vm | Start-VM -confirm:$false | Out-Null
+    }
+    else{
+        Write-Host $vm "is already powered on" -fore Red
+    }
+}
+
+Function Set-NetworkInterface{
+    param($virtualmachine)
+    $vm = Get-VM $virtualmachine -ErrorAction 0
+    $dhcp = $dhcp.ToLower()
+    $toolsStatus = (Get-VM $vm | Get-View).Guest.ToolsStatus
+    if($vm.powerstate -eq "PoweredOn"){
+        if($toolsStatus -eq "toolsOk"){
+ 
+            # Create the DNS entries in /etc/resolv.conf if they don't exist
+            # TODO: Convert in to loop for array of DNS servers
+            if($dns1 -ne ""){
+            $script = 'grep #dns1# /etc/resolv.conf 2>&1; if [ $? -ne 0 ]; then echo "nameserver #dns1#" >> /etc/resolv.conf; fi'
+            $script = $script.Replace("#dns1#", $dns1)
+            Write-Host "Configuring DNS $dns1" -fore Yellow
+            $vm | Invoke-VMScript -GuestUser $gu -GuestPassword $gp $script
+            }
+
+            # Add IP Address information to eth0
+            # TODO: Create array for multiple network interfaces
+            $script = 'ETH=/etc/sysconfig/network-scripts/ifcfg-eth0;'
+            if($dhcp -eq "false"){
+                $script += ' grep ^BOOTPROTO "$ETH" 2>&1; if [ $? -ne 0 ]; then echo "BOOTPROTO=none" >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i "s/^BOOTPROTO=.*$/BOOTPROTO=none/" "$ETH"; fi;'
+                if($ip -ne ""){
+                    $script += ' grep ^IPADDR "$ETH" 2>&1; if [ $? -ne 0 ]; then echo "IPADDR=#ip#" >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i "s/^IPADDR=.*$/IPADDR=#ip#/" "$ETH"; fi;'
+                    $script = $script.Replace("#ip#", $ip)
+                }
+                if($nm -ne ""){
+                    $script += ' grep ^NETMASK "$ETH" 2>&1; if [ $? -ne 0 ]; then echo "NETMASK=#nm#" >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i "s/^NETMASK=.*$/NETMASK=#nm#/" "$ETH"; fi;'
+                }
+                if($gw -ne ""){
+                    $script += ' grep ^GATEWAY "$ETH" 2>&1; if [ $? -ne 0 ]; then echo "GATEWAY=#gw#" >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i "s/^GATEWAY=.*$/GATEWAY=#gw#/" "$ETH"; fi;'
+                }
+                if(($ip -ne "") -or ($nm -ne "") -or ($gw -ne "")){
+                    Write-Host "Configuring Network Interface for $vm" -fore Yellow
+                    $vm | Invoke-VMScript -GuestUser $gu -GuestPassword $gp $script
+                }
+            }
+            else{
+                $script += ' grep ^BOOTPROTO "$ETH" 2>&1; if [ $? -ne 0 ]; then echo "BOOTPROTO=none" >> /etc/sysconfig/network-scripts/ifcfg-eth0; else sed -i "s/^BOOTPROTO=.*$/BOOTPROTO=none/" "$ETH"; fi;'
+                Write-Host "Configuring Network Interface for $vm" -fore Yellow
+                $vm | Invoke-VMScript -GuestUser $gu -GuestPassword $gp $script
+            }
+        }
+        else{
+            Write-Host $vm "VMware Tools are out of date or not running" -fore Red
+            }
+    }
+    else{
+        Write-Host $vm "is not running" -fore Red
+    }
+}
+
 # Connect to ESXi Host
 Write-Host "Connecting to ESXi Host..."
-Connect-VIServer -Server $vmHost
+Connect-VIServer -Server $vmhost
 
 # Populate list and settings for each VM
-$vmList = Import-CSV -Path $vmCsvFile
+$guests = Import-CSV -Path $csvfile
 
-ForEach ($vm in $vmList) {
+ForEach ($vms in $guests) {
+    $vh = $vmhost
+    $vm = $vms.VM_Name
+    $dhcp = $vms.Boolean_DHCP
+    $ip = $vms.Static_IP
+    $nm = $vms.Netmask
+    $gw = $vms.Default_GW
+    $dns1 = $vms.Primary_DNS
 
   # Ensure VM name is defined
-  if ($vm.VM_Name -ne "") {
-
-    # Verify if VM already exists
-    $noVM = Get-VM -Name $vm.VM_Name -ErrorAction 0
-    if (!($noVM)) {
-
-      # Begin VM import process
-      Write-Host "Deploying $($vm.VM_Name)..."
-      Import-VApp -Source $vmOvf -Name $vm.VM_Name -VMHost $vmHost -Datastore $vmDatastore -DiskStorageFormat $vmDiskFormat
-
-      # Start up the VM and wait for VMware Guest Tools to start
-      Write-Host "Starting $($vm.VM_Name)..."
-      Start-VM -VM $vm.VM_Name -confirm:$false | Out-Null
-      $timeOut = New-Timespan -Seconds $maxTimeOut
-      $loops = 0
-      Do {
-        Start-Sleep -Seconds 10
-        $vmStatus = (Get-VM -Name $vm.VM_Name).ExtensionData.Guest.ToolsStatus
-      } Until (($vmStatus -match 'toolsOk') -Or ($loops -gt $maxTimeOut))
-
-      # Need to allow the services to start up
-      Write-Host "$($vm.VM_Name) is now started. Waiting for services to come up."
-      Start-Sleep -Seconds 30
-
-      # Set up network configuration on system
-      $script = 'echo "DHCP=#DHCP#";echo "IP=#IP#"; echo "NM=#NM#"; echo "GW=#GW#"; echo "DNS1=#DNS1#"; DHCP=#DHCP#; if [ "$DHCP" = "false" ]; then echo "IPADDR=#IP#" >> /etc/sysconfig/network-scripts/ifcfg-eth0; echo "NETMASK=#NM#" >> /etc/sysconfig/network-scripts/ifcfg-eth0; echo "GATEWAY=#GW#" >> /etc/sysconfig/network-scripts/ifcfg-eth0; sed -i "s/^BOOTPROTO=.*$/BOOTPROTO=static/" /etc/sysconfig/network-scripts/ifcfg-eth0; echo "" > /etc/resolv.conf; echo "nameserver #DNS1#" >> /etc/resolv.conf; else sed -i "s/^BOOTPROTO=.*$/BOOTPROTO=dhcp/" /etc/sysconfig/network-scripts/ifcfg-eth0; fi; /sbin/ifdown eth0; /sbin/ifup eth0;'
-
-      $script = $script.Replace("#DHCP#", $vm.Boolean_DHCP).Replace("#IP#", $vm.Static_IP).Replace("#NM#", $vm.Netmask)
-      $script = $script.Replace("#GW#", $vm.Default_GW).Replace("#DNS1#", $vm.Primary_DNS)
-      Write-Host "Setting up Virtual Machine network settings."
-      $provision = Invoke-VMScript -VM $vm.VM_Name -GuestUser $vmGuestLogin -GuestPassword $vmGuestPassword -ScriptType Bash -ScriptText $script
-      $provision.ScriptOutput
-
-    }
-    else {
-      Write-Host "Virtual Machine already exists. Nothing to do." -ForegroundColor Yellow
-    }
+  if ($vm -ne "") {
+    Deploy-OVF $vm
+    Start-VM $vm
+    Set-NetworkInterface $vm
   }
   else {
-    Write-Host "Virtual Machine name is not defined." -ForegroundColor Red
+    Write-Host "Virtual Machine name is not defined." -fore Red
   }
 }
 
-Write-Host "All Virtual Machines have been deployed." -ForegroundColor Green
+Write-Host "All Virtual Machines have been deployed." -fore Green
 Write-Host "Disconnecting from ESXi Host..."
 Disconnect-VIServer -Confirm:$false
